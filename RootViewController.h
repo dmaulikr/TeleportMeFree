81f684022afea2346d4ace5d38ff85fdfbd52758
jalbatross <ih8u2o@gmail.com>
2016-08-26 10:48:16 -0700

    Text validation protoype with REGEX

--- RootViewController.h
+++ RootViewController.h
@@ -1,3 +1,9 @@
+typedef enum {
+    LONGITUDE,
+    LATITUDE,
+    ALTITUDE
+} COORDINATE_TYPE;
+
 @interface RootViewController: UIViewController<UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate> {
     UILabel*helloLabel;
     UITableView*tableView;
--- RootViewController.mm
+++ RootViewController.mm
@@ -4,11 +4,11 @@
 - (void)loadView {
 	self.view = [[[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]] autorelease];
 	self.view.backgroundColor = [UIColor whiteColor];
-        helloLabel = [[UILabel alloc] initWithFrame:CGRectMake(21,0,self.view.frame.size.width,44)];
-        helloLabel.text = @"Latitude";
-        helloLabel.backgroundColor = [UIColor clearColor];
-        helloLabel.textAlignment = UITextAlignmentLeft;
-        [self.view addSubview:helloLabel];
+	helloLabel = [[UILabel alloc] initWithFrame:CGRectMake(21,0,self.view.frame.size.width,44)];
+	helloLabel.text = @"Latitude";
+	helloLabel.backgroundColor = [UIColor clearColor];
+	helloLabel.textAlignment = UITextAlignmentLeft;
+	[self.view addSubview:helloLabel];
 
 // init table view
     tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
@@ -32,7 +32,7 @@
 - (NSInteger)tableView:(UITableView *)theTableView numberOfRowsInSection:(NSInteger)section
 {
     if (section == 0)
-        return 3;
+	return 3;
     else return 1;
 }
 
@@ -43,59 +43,62 @@
     UITableViewCell *cell = (UITableViewCell*) [theTableView dequeueReusableCellWithIdentifier:kCellIdentifier];
     
     if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault 
-                                   reuseIdentifier:kCellIdentifier] autorelease];
-        cell.accessoryType = UITableViewCellAccessoryNone;
+	cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault 
+				   reuseIdentifier:kCellIdentifier] autorelease];
+	cell.accessoryType = UITableViewCellAccessoryNone;
 
-        if ([indexPath section] == 0) { //Section for number inputs
-            UITextField *playerTextField = [[UITextField alloc] initWithFrame:CGRectMake(110, 10, 185, 30)];
-            playerTextField.adjustsFontSizeToFitWidth = YES;
-            playerTextField.textColor = [UIColor blackColor];
-            switch ([indexPath row]) {
-                case 0:
-                    playerTextField.placeholder = @"Latitude";
-                    playerTextField.keyboardType = UIKeyboardTypeDecimalPad;
-                    playerTextField.returnKeyType = UIReturnKeyNext;
-                    break;
-                case 1:
-                    playerTextField.placeholder = @"Longitude";
-                    playerTextField.keyboardType = UIKeyboardTypeDefault;
-                    playerTextField.keyboardType = UIKeyboardTypeDecimalPad;
-                    playerTextField.returnKeyType = UIReturnKeyNext;
-                    break;
-                case 2:
-                    playerTextField.placeholder = @"Altitude";
-                    playerTextField.keyboardType = UIKeyboardTypeDecimalPad;
-                    break;
-                case 3:
-                    playerTextField.placeholder = @"ERROR";
-                    break;
-            }
-            
-            playerTextField.backgroundColor = [UIColor whiteColor];
-            playerTextField.autocorrectionType = UITextAutocorrectionTypeNo; // no auto correction support
-            playerTextField.autocapitalizationType = UITextAutocapitalizationTypeNone; // no auto capitalization support
-            playerTextField.textAlignment = UITextAlignmentLeft;
-            playerTextField.tag = 0;
-            //playerTextField.delegate = self;
+	if ([indexPath section] == 0) { //Section for number inputs
+	    UITextField *playerTextField = [[UITextField alloc] initWithFrame:CGRectMake(110, 10, 185, 30)];
+	    playerTextField.adjustsFontSizeToFitWidth = YES;
+	    playerTextField.textColor = [UIColor blackColor];
+	    switch ([indexPath row]) {
+		case 0:
+		    playerTextField.placeholder = @"Latitude";
+		    playerTextField.keyboardType = UIKeyboardTypeDecimalPad;
+		    playerTextField.returnKeyType = UIReturnKeyNext;
+            playerTextField.tag = LATITUDE;
+		    break;
+		case 1:
+		    playerTextField.placeholder = @"Longitude";
+		    playerTextField.keyboardType = UIKeyboardTypeDefault;
+		    playerTextField.keyboardType = UIKeyboardTypeDecimalPad;
+		    playerTextField.returnKeyType = UIReturnKeyNext;
+            playerTextField.tag = LONGITUDE;
+		    break;
+		case 2:
+		    playerTextField.placeholder = @"Altitude";
+		    playerTextField.keyboardType = UIKeyboardTypeDecimalPad;
+            playerTextField.tag = ALTITUDE;
+		    break;
+		case 3:
+		    playerTextField.placeholder = @"ERROR";
+		    break;
+	    }
+	    
+	    playerTextField.backgroundColor = [UIColor whiteColor];
+	    playerTextField.autocorrectionType = UITextAutocorrectionTypeNo; // no auto correction support
+	    playerTextField.autocapitalizationType = UITextAutocapitalizationTypeNone; // no auto capitalization support
+	    playerTextField.textAlignment = UITextAlignmentLeft;
+	    playerTextField.tag = 0;
+	    //playerTextField.delegate = self;
 
-            playerTextField.clearButtonMode = UITextFieldViewModeNever; // no clear 'x' button to the right
-            [playerTextField setEnabled: YES];
-            [cell.contentView addSubview:playerTextField];
-            playerTextField.delegate = self;
-            [playerTextField release];
+	    playerTextField.clearButtonMode = UITextFieldViewModeNever; // no clear 'x' button to the right
+	    [playerTextField setEnabled: YES];
+	    [cell.contentView addSubview:playerTextField];
+	    playerTextField.delegate = self;
+	    [playerTextField release];
     }
     
     else { //Section for boolean inputs (UISwitches)
-        UISwitch *onSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(80, 10, 185, 30)];
-        [onSwitch setOnTintColor:[UIColor redColor]];
-        
-        if ([indexPath row] == 0) {
-            cell.textLabel.text = @"Teleport!";
-        }
-        else {
-            cell.textLabel.text = @"ERROR!!";
-        }
+	UISwitch *onSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(120, 10, 185, 30)];
+	[onSwitch setOnTintColor:[UIColor redColor]];
+	
+	if ([indexPath row] == 0) {
+	    cell.textLabel.text = @"Teleport!";
+	}
+	else {
+	    cell.textLabel.text = @"ERROR!!";
+	}
 
     [cell.contentView addSubview:onSwitch];
     [onSwitch release];
@@ -104,33 +107,56 @@
 
 if ([indexPath section] == 0) { // Latitude and Longitude
     switch ([indexPath row]) {
-        case 0: 
-            cell.textLabel.text = @"Latitude"; 
-            break;
-        case 1: 
-            cell.textLabel.text = @"Longitude"; 
-            break;
-        case 2: 
-            cell.textLabel.text = @"Altitude"; 
-            break;
-        default:
-            cell.textLabel.text = @"ERROR";
-            break;
+	case 0: 
+	    cell.textLabel.text = @"Latitude"; 
+	    break;
+	case 1: 
+	    cell.textLabel.text = @"Longitude"; 
+	    break;
+	case 2: 
+	    cell.textLabel.text = @"Altitude"; 
+	    break;
+	default:
+	    cell.textLabel.text = @"ERROR";
+	    break;
     }
 }
-return cell;    
+return cell;	
 }
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
-    NSLog(@"This function was called to change text");
-    // Prevent crashing undo bug – see note below.
-    if(range.length + range.location > textField.text.length)
+    NSString *expression = @"^([0-9]*)(\\.([0-9]+)?)?$";
+    NSString *newStr = textField.text;
+
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:expression options:NSRegularExpressionCaseInsensitive error:nil];
+    NSUInteger noOfMatches = [regex numberOfMatchesInString:newStr options:0 range:NSMakeRange(0,[newStr length])];
+
+    if (noOfMatches==0)
     {
         return NO;
     }
 
     NSUInteger newLength = [textField.text length] + [string length] - range.length;
-    return newLength <= 5;
+    if(range.length + range.location > textField.text.length)
+    {
+        return NO;
+    }
+    if ([newStr containsString:@"."])
+    {
+        return newLength <= 6;
+    }
+    return newLength <= 3;
+
+    //  return YES;
+    //NSUInteger newLength = [textField.text length] + [string length] - range.length;
+    //if (newLength > 12)
+	//return NO;
+
+     //if more than one decimal
+     //     return NO;
+
+
+     return YES;
 }
 #pragma mark - UITableViewDelegate
 // when user tap the row, what action you want to perform
